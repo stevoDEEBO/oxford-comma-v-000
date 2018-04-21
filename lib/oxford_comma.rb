22e5946cd9901c6f7b26b746fd76afad4a9c4d56
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-21 11:38:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,16 @@
 def oxford_comma(array)
-  if array.length == 1
-    return array.join
-  else #array.length == 2
-    "#{array[0..-2].join(", ")} and #{array.last}"
-    #array[0..-2].join(", ") + " and " + array[-1]
-  else
-    array[-1] = "and #{array[-1]}"
-    array.join(', ')
-  end
+  #if array.length == 1
+  #  return array.join
+  #else #array.length == 2
+  #  "#{array[0..-2].join(", ")} and #{array.last}"
+  #else
+  #  array[-1] = "and #{array[-1]}"
+  #  array.join(', ')
+  #end
 
-  array.each do |array|   
+  array.each do |array|
     array[-1] = "and #{array[-1]}"                      
     array[0..-2].join(", ")
-    
+  end
+
 end
