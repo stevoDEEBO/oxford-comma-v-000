5684e059901059cd61760b2dd63f8d9cd5bc687f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-21 11:21:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     "#{array[0..-2].join(", ")} and #{array.last}"
     #array[0..-2].join(", ") + " and " + array[-1]
-  #else
-
+  else
+    array[-1] = "and #{array[-1]}"
+ array.join(', ')
   end
 end
