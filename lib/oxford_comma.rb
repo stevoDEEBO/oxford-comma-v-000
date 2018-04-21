c8496bce6c7be51e01926784503b2bcba98b9485
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-21 11:59:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   #if array.length == 1
   #  return array.join
   if array.length == 2
-    return "#{array[0..-2].join(", ")} and #{array.last}"
+    return "#{array[0]} and #{array.last}"
   else
     array[-1] = "and #{array[-1]}"
     array.join(', ')
